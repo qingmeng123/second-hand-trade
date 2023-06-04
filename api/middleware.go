ba97aa7efe794f20b0172d8ca59688ee576fb83b
qingmeng123 <2379224042@qq.com>
2023-06-04 18:25:09 +0800

    debug

--- api/middleware.go
+++ api/middleware.go
@@ -23,7 +23,7 @@ func Cors() gin.HandlerFunc {
 		method := context.Request.Method
 		context.Header("Access-Control-Allow-Origin", "*")
 		context.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
-		context.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		context.Header("Access-Control-Allow-Methods", "POST,PUT,GET, OPTIONS")
 		context.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		context.Header("Access-Control-Allow-Credentials", "true")
 		if method == "OPTIONS" {
